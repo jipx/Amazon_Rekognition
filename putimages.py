49dd5a2ae1e35effe1cdbdcf033756dc410e9e3c
EC2 Default User <ec2-user@ip-172-31-31-59.ec2.internal>
2024-09-17 06:42:01 +0000

    set path to images

--- putimages.py
+++ putimages.py
@@ -1,19 +1,29 @@
 import boto3
+import os
 
 s3 = boto3.resource('s3')
 
 # Get list of objects for indexing
-images=[('image01.jpeg','M S Dhoni'),
-      ('image02.jpeg','M S Dhoni'),
-      ('image03.jpeg','Sachin'),
-      ('image04.jpeg','Sachin'),
-      ('image05.jpeg','Virat'),
-      ('image06.jpeg','Virat')
-      ]
+images = [
+    ('image01.jpeg', 'M S Dhoni'),
+    ('image02.jpeg', 'M S Dhoni'),
+    ('image03.jpeg', 'Sachin'),
+    ('image04.jpeg', 'Sachin'),
+    ('image05.jpeg', 'Virat'),
+    ('image06.jpeg', 'Virat')
+]
 
-# Iterate through list to upload objects to S3   
+# Ensure the images are in the "images" folder under the current directory
+image_path = os.path.join(os.getcwd(), 'images')
+
+
+# Iterate through list to upload objects to S3
 for image in images:
-    file = open(image[0],'rb')
-    object = s3.Object('sportsperson-images','index/'+ image[0])
-    ret = object.put(Body=file,
-                    Metadata={'FullName':image[1]})
+    file_path = os.path.join(image_path, image[0])
+    try:
+        with open(file_path, 'rb') as file:
+            object = s3.Object('sportsperson-images-jipx', 'index/' + image[0])
+            ret = object.put(Body=file, Metadata={'FullName': image[1]})
+            print(f"Uploaded {image[0]} successfully.")
+    except FileNotFoundError:
+        print(f"File {file_path} not found.")
